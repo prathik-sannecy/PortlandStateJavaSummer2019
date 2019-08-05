530dd195ac58ba60aca1ee0d08af3b526ea018a0
prathik <prathik@pdx.edu>
2019-08-04 21:11:00 -0700

    Updated most recent files from Dave, including creating new window

--- ApptBookAndroid/app/src/main/java/edu/pdx/cs410J/prathik/ApptBookAndroid/MainActivity.java
+++ ApptBookAndroid/app/src/main/java/edu/pdx/cs410J/prathik/ApptBookAndroid/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.pdx.cs410J.prathik.ApptBookAndroid;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextClock;
 import android.widget.TextView;
-public class MainActivity extends AppCompatActivity {
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final int DOUBLE_NUMBER = 1;
 
     private int count;
+    private String messageToDisplayAfterResume;
+    private Integer doubled = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +38,28 @@ public class MainActivity extends AppCompatActivity {
 
         displayCount();
 
+        ListView listView = findViewById(R.id.listView);
+        String[] daysOfTheWeek = getResources().getStringArray(R.array.days_of_the_week);
+        listView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, daysOfTheWeek));
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementCount();
+                displayCount();
+            }
+        });
     }
 
     private void displayCount() {
+        String message = "The count is " + count;
+        setMessage(message);
+    }
+
+    private void setMessage(CharSequence message) {
         TextView text = findViewById(R.id.text);
-        text.setText("Hello" + count);
+        text.setText(message);
     }
 
     @Override
@@ -38,11 +70,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
-        this.count++;
+        incrementCount();
         outState.putInt("count", this.count);
 
         super.onSaveInstanceState(outState);
+    }
+
+    private void incrementCount() {
+        this.count++;
     }
 
     @Override
@@ -51,6 +92,57 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             this.count = savedInstanceState.getInt("count");
+        }
+    }
+
+    public void displayCurrentTimeInEditText(View view) {
+        TextClock clock = (TextClock) view;
+        setMessage(clock.getText());
+    }
+
+    public void doubleNumber(View view) {
+        EditText numberToDouble = findViewById(R.id.number);
+        String numberAsString = numberToDouble.getText().toString();
+        try {
+            int number = Integer.parseInt(numberAsString);
+            Uri uri = Uri.fromParts("number", String.valueOf(number), null);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri, this, DoubleNumberActivity.class);
+            intent.putExtra("number", number);
+            startActivityForResult(intent, DOUBLE_NUMBER);
+
+        } catch (NumberFormatException ex) {
+            toast("Bad number: " + numberAsString);
+        }
+    }
+
+    private void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == DOUBLE_NUMBER) {
+            if (resultCode == RESULT_OK) {
+                int number = data.getIntExtra("number", 0);
+                doubled = data.getIntExtra("doubled", 0);
+                messageToDisplayAfterResume = String.format("%d doubled is %d", number, doubled);
+                // Changing the state of the widgets here will have no effect because the
+                // activity is "paused"
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (messageToDisplayAfterResume != null) {
+            setMessage(messageToDisplayAfterResume);
+        }
+
+        if (doubled != null) {
+            EditText number = findViewById(R.id.number);
+            number.setText(String.valueOf(doubled));
         }
     }
 }
